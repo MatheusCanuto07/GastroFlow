4ef5794e1c022570348c3f2c4e53a52d8e32030c
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-06-14 16:27:43 -0300

    initial developer

--- src/src/lib/components/Nav.svelte
+++ src/src/lib/components/Nav.svelte
@@ -3,15 +3,19 @@
   // location = $page.url.pathname;
 </script>
 
-<div class="navbar mb-3 bg-base-100 shadow-sm lg:px-40 xl:px-60">
+<div class="navbar mb-3 bg-base-100 shadow-sm lg:px-40 xl:px-60" style="background-color: #f1c42d;">
 	<div class="flex-1">
-		<a href="/home" class="btn btn-ghost text-xl">GastroFlow</a>
+    <div class="w-24">
+      <a href="/home" class="">
+        <img class="h-auto" src="/logo.jpeg" alt="Logo Gastro Flow">
+      </a>
+    </div>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal items-center px-1">
 			<li><a href="/home/insumo">Insumo</a></li>
 			<li><a href="/home/produto">Produto</a></li>
-			<li><a href="/home/receita">Receita</a></li>
+			<li><a href="/home/receita">Ficha Técnica</a></li>
 			<li><a href="/home/fornecedor">Fornecedor</a></li>
 		</ul>
 	</div>
--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
 	},
 
   daisyui: {
-    themes: ["light", "dark", "cupcake", "retro"],
+    themes: ["light", "dark", "cupcake", "retro", "bumblebee"],
   },
 
 	plugins: [
